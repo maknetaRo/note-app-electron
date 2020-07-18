4356f5323e90c0bf7da704073fee9cc87f4e742c
Magda <makneta@gmail.com>
2020-07-18 23:41:53 +0200

    add save html and same file

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,13 @@
-const marked = require('marked');
 const { remote, ipcRenderer } = require('electron');
-const mainProcess = remote.require('./index.js')
+const mainProcess = remote.require('./index.js');
+const currentWindow = remote.getCurrentWindow();
+const path = require('path');
+
+let filePath = null;
+let originalContent = "";
+
+
+const marked = require('marked');
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -13,20 +20,61 @@ const showFilebutton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
 const renderMarkdownToHtml = (markdown) => {
-    htmlView.innerHTML = marked(markdown, { sanitize: true })
-}
+    htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
 
 markdownView.addEventListener('keyup', (event) => {
     const currentContent = event.target.value;
     renderMarkdownToHtml(currentContent);
+    updateUserInterface(currentContent !== originalContent);
+});
+
+newFileButton.addEventListener('click', () => {
+    mainProcess.createWindow();
 })
 
 openFileButton.addEventListener('click', () => {
-    mainProcess.getFileFromUser();
+    mainProcess.getFileFromUser(currentWindow);
 });
 
+saveHtmlButton.addEventListener('click', () => {
+    mainProcess.saveHtml(currentWindow, htmlView.innerHTML);
+});
+
+saveMarkdownButton.addEventListener('click', () => {
+    mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value);
+})
+
+revertButton.addEventListener('click', () => {
+    markdownView.value = originalContent;
+    renderMarkdownToHtml(originalContent);
+})
+
+const updateUserInterface = (isEdited) => {
+    let title = "Note Taking App"
+    if (filePath) {
+        title = `${path.basename(filePath)} - ${title}`;
+    }
+    if (isEdited) {
+        title = `${title} (Edited)`;
+    }
+    currentWindow.setTitle(title);
+    currentWindow.setDocumentEdited(isEdited);
+
+    saveMarkdownButton.disabled = !isEdited;
+    revertButton.disabled = !isEdited;
+}
+
 ipcRenderer.on('file-opened', (event, file, content) => {
+    filePath = file;
+    originalContent = content;
+
     markdownView.value = content;
     renderMarkdownToHtml(content);
+
+    updateUserInterface();
 });
+
+
+
 
--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const { app, BrowserWindow, dialog } = require('electron');
 
 const fs = require('fs');
+const windows = new Set();
 
-const getFileFromUser = exports.getFileFromUser = () => {
-    dialog.showOpenDialog(mainWindow, {
-        properties: ['openFile'],
-        filters: [
-            { name: 'Text Files', extensions: ['txt'] },
-            { name: 'Markdown Files', extensions: ['md', 'markdown'] }
-        ]
-    }).then(result => {
-        const file = result.filePaths[0];
-        const content = fs.readFileSync(file).toString();
-        mainWindow.webContents.send('file-opened', file, content);
-    }).catch(err => {
-        console.log(err)
-    });
 
-}
+const createWindow = exports.createWindow = () => {
+    let x, y;
 
-let mainWindow;
+    const currentWindow = BrowserWindow.getFocusedWindow();
 
-const createMainWindow = () => {
-    mainWindow = new BrowserWindow({
-        title: "Note taking app",
+    if (currentWindow) {
+        const [currentWindowX, currentWindowY] = currentWindow.getPosition();
+        x = currentWindowX + 15;
+        y = currentWindowY + 15;
+    }
+
+    let newWindow = new BrowserWindow({
+        x, y,
         width: 800,
         height: 600,
         resizable: true,
@@ -32,23 +25,101 @@ const createMainWindow = () => {
             nodeIntegration: true,
             enableRemoteModule: true
         }
-    })
+    });
 
-    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
-}
+    newWindow.loadURL(`file://${__dirname}/app/index.html`)
+    newWindow.once('ready-to-show', () => {
+        newWindow.show();
+    });
+
+    newWindow.on('closed', () => {
+        windows.delete(newWindow);
+        newWindow = null;
+    });
+
+    windows.add(newWindow);
+    return newWindow;
+};
+app.on('window-all-clsed', () => {
+    if (process.platform === 'darwin') {
+        return false;
+    }
+    app.quit();
+});
 
 app.on('ready', () => {
-    createMainWindow();
+    createWindow();
+});
 
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-        // getFileFromUser();
+app.on('activate', (event, hasVisibleWindows) => {
+    if (!hasVisibleWindows) { createWindow(); }
+})
 
-    })
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    })
+app.on('will-finish-launching', () => {
+    app.on('open-file', (event, file) => {
+        const win = createWindow();
+        win.once('ready-to-show', () => {
+            openFile(win, file);
+        });
+    });
 });
 
 
+const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
+    dialog.showOpenDialog(targetWindow, {
+        properties: ['openFile'],
+        filters: [
+            { name: 'Text Files', extensions: ['txt'] },
+            { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+        ]
+    }).then(result => {
+
+        const file = result.filePaths[0];
+        const content = fs.readFileSync(file).toString();
+        app.addRecentDocument(file);
+        targetWindow.setRepresentedFilename(file);
+        targetWindow.webContents.send('file-opened', file, content);
+    }).catch(err => {
+        console.log(err)
+    });
+
+}
+
+const saveHtml = exports.saveHtml = (targetWindow, content) => {
+    dialog.showSaveDialog(targetWindow, {
+        title: 'Save HTML',
+        defaultPath: app.getPath('documents'),
+        filters: [
+            {
+                name: 'HTML Files', extensions: ['html', 'htm']
+            }
+        ]
+    }).then(result => {
+        const filePath = result.filePath;
+        console.log(result)
+        console.log(filePath)
+        console.log(content)
+        fs.writeFileSync(filePath, content);
+    }).catch(error => {
+        console.log(error);
+    });
+
+};
+
+
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+    dialog.showSaveDialog(targetWindow, {
+        title: "Save Markdown",
+        defaultPath: app.getPath('documents'),
+        filters: [
+            { name: 'Markdown', extensions: ['md', 'markdown'] }
+        ]
+    }).then(result => {
+        // const file = result.filePath;
+        fs.writeFileSync(file, content);
+        // openFile(targetWindow, file);
+    }).catch(error => {
+        console.log(error);
+    })
+}
